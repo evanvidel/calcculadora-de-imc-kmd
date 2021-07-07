7acb1228653e03c82f3d97e065818816775a60ef
evanvidel <evanvidel@gmail.com>
2021-07-07 11:33:20 -0300

    creating IMC calculation

--- app/src/main/java/com/axweb/calculadoradeimc/MainActivity.kt
+++ app/src/main/java/com/axweb/calculadoradeimc/MainActivity.kt
@@ -2,10 +2,60 @@ package com.axweb.calculadoradeimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.axweb.calculadoradeimc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val btCalcular = binding.btCalculate
+        val messagem = binding.message
+
+        btCalcular.setOnClickListener {
+            val editPeso = binding.editWeight.text.toString()
+            val editAltura = binding.editHeight.text.toString()
+
+            if (editPeso.isEmpty()) {
+                messagem.setText("Informe o seu Peso")
+            } else if (editAltura.isEmpty()) {
+
+                messagem.setText("Informe o sua Altura")
+            }else{
+                calculateIMC()
+            }
+        }
+    }
+
+    private fun calculateIMC() {
+        val pesoID = binding.editWeight
+        val alturaID = binding.editHeight
+
+        val peso = Integer.parseInt(pesoID.text.toString())
+        val altura = java.lang.Float.parseFloat(alturaID.text.toString())
+        val resultado = binding.message
+        val imc = peso /(altura * altura)
+
+        val message = when {
+            imc <= 18.5 -> "Peso Baixo"
+            imc <= 24.9 -> "Peso Normal"
+            imc <= 29.9 -> "Sobrepeso"
+            imc <= 34.9 -> "Obesidade (grau 1)"
+            imc <= 39.9 -> "Obesidade (Grau 2)"
+            else -> "Obesidade Mórbida (Grau 3)"
+        }
+        imc.toString()
+        resultado.setText("IMC: $imc \n $message")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+
+        return true
     }
 }
